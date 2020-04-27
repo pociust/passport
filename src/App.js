b9872316c1d0eab06e768c8ae3337567ad899982
Tom Pocius <poci14@gmail.com>
2020-04-27 14:52:01 -0500

    basic react router home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 
 function App() {
   return (
-    <div>
-      helloworld
-      <button>click me</button>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path ="/">
+            <Home></Home>
+          </Route>
+          <Route exact path ="/login">
+            <Login></Login>
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Home = () => {
+
+  return (
+    <div>
+      hello home
+    </div>
+  )
+}
+
+
+export default Home;
