411d3e95a852f9369fc8255ea3b98d0216884647
Tom Pocius <poci14@gmail.com>
2020-05-01 15:24:22 -0500

    added mongo db

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/user_Name')
+
 
 app.listen(PORT, function () {
   console.log(`app listenting on ${PORT}`)
