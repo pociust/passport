4119565b2161361a247fbf1828e86667b9301a36
Tom Pocius <poci14@gmail.com>
2020-05-05 22:06:14 -0500

    addeed function to get username and password

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -8,15 +8,21 @@ import Strength from "../Components/Strength";
 const Home = () => {
   const [state, setState] = useState('');
 
-  const formInput = event => {
+  const formInput = (event) => {
     event.preventDefault();
-    console.log('1111', event.target.value.length)
+    console.log('22222', event.target.userName.value, event.target.password.value)
+  }
+
+  const passwordStrength = (event) => {
+    event.preventDefault();
+    console.log('1111', event.target.value)
+
   }
 
   return (
     <div className="frow-container p-10">
       <div>{state}</div>
-      <Form formInput={formInput}></Form>
+      <Form formInput={formInput} passwordStrength={passwordStrength}></Form>
       <Strength></Strength>
       <NavLink to="/signup">Log In</NavLink>
 
