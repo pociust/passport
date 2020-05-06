5300d45ce6fb0b6e758109703edb935379f11ec3
Tom Pocius <poci14@gmail.com>
2020-05-05 22:19:41 -0500

    added cors to backend

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const routes = require('./routes');
+const cors = require("cors");
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+app.use(cors());
 app.use(routes);
+
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/user_Name')
 
