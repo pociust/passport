ffa55c45335cdd5a344337a4a6e41e80c20623ab
Tom Pocius <poci14@gmail.com>
2020-05-05 22:05:48 -0500

    added input tag for submit and passed username and password back to parent

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,9 +3,10 @@ import React from "react";
 const Form = (props) => {
   return (
     <>
-      <form className="home-form" id="homeForm">
-        <input type="text" placeholder="name"></input>
-        <input type="password" placeholder="password" onChange={props.formInput}></input>
+      <form className="home-form" id="homeForm" onSubmit={props.formInput}>
+        <input type="text" placeholder="name" name="userName"></input>
+        <input type="password" placeholder="password" name="password" onChange={props.passwordStrength}></input>
+        <input type="submit"></input>
       </form>
 
     </>
