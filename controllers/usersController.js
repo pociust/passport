9bad712fefced4f591b9915055da27a33970eabd
Tom Pocius <poci14@gmail.com>
2020-05-05 19:33:59 -0500

    added create controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,7 +4,12 @@ module.exports = {
   //CRUD
 
   //create
-
+  create: function (req, res) {
+    db.user_Name
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err))
+  }
   //read
 
   //update
